e016ce6b3d8a6ae6854cf9a9f8fb0bf4d3d2d86d
Reed Monkey <shyaminreallife@gmail.com>
2025-01-30 13:12:48 +0545

    async js revist

---
+++ api/async.js
@@ -0,0 +1,34 @@
+// async and await makes promisies easier to write
+// async makes functiion return the promise
+//await mkakes  function return the  promise
+
+//high order function in the js
+//A high-order function is a function that either takes one or more functions as arguments, returns a function, or both.
+//
+//
+//
+//
+
+// const coding = ["js", "ruby", "java",  "python"]
+
+// const values  = coding.forEach ((item) => {
+//     return item
+// })
+
+// const mynum = [1,2,3,4,5,6]
+
+//  const newnums = mynum.filter((num) => num >2)
+
+//  console.log(newnums)
+
+
+//  const mynum = [1,2,3,4,5,6]
+
+ 
+//  const newnums = mynum.filter((num) => {
+//     return num > 4
+
+//  })
+
+//   console.log(newnums)
+
