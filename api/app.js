5ee276bf1e30bf7607fc7b697728b8c32684c0d9
Reed Monkey <shyaminreallife@gmail.com>
2025-02-03 15:01:31 +0545

    ..

--- api/app.js
+++ api/app.js
@@ -1,18 +1,28 @@
- const express = require(express)
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
- const app = express()
+const app = express()
 
-//routes imported
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-import userRouter from "./routes/user.route.js"
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
+
+//routes import
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
 
-app.use("/api/V1/users", userRouter)
+app.use("/api/v1/users", userRouter)
 
 
+// http://localhost:8000/api/v1/users/register
 
-//https:localhost:8000/api/v1/users/register
-//https:localhost:8000/api/v1/users/login
- export {app}
+export { app }
