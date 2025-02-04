f015e314c356f57434d583f614f5c1e1f7f8d5f8
Reed Monkey <shyaminreallife@gmail.com>
2025-02-04 23:48:17 +0545

    ..

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -252,7 +252,8 @@ const changeCurrentPassword =  asyncHandler(async(req,res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
   return res
   .status(200)
-  .json(200, req.user, "current user fetched sucesfully")
+  .json(new ApiResponse(
+    200, req.user, "current user fetched sucesfully"))
 })
 
 
@@ -264,7 +265,7 @@ const updateAccoutnDetails = asyncHandler(async(req, res) => {
     throw new ApiError(400,  "All fields are required")
   }
 
- const user =  User.findByIdAndUpdate(
+ const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
