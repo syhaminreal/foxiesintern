a53bd8898bd536011d4d0c8bc3005172bd10590e
Reed Monkey <shyaminreallife@gmail.com>
2025-02-10 00:04:20 +0545

    </>

--- test/index.js
+++ test/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { connectDb } from "./db/connectDB.js";
+import  authRoutes from "./routes/auth.route.js"
+
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World! 12");
 });
+
+app.use("/api/auth", authRoutes)
 
 app.listen(3000, () => {
   connectDb();
