4c36379199309519aaf575148191ec9fd3711253
Reed Monkey <shyaminreallife@gmail.com>
2025-02-10 11:44:22 +0545

    ..post man not working?

--- test/controllers/auth.controller.js
+++ test/controllers/auth.controller.js
@@ -1,11 +1,52 @@
+import { User } from '../models/user.model.js';
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"; 
+
 export const signup = async (req, res) => {
- // res.send("signup route")
-}
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24hrs
+    });
+
+    await user.save();
+
+    // Generate JWT token and set cookie
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 export const login = async (req, res) => {
-    res.send("login route")
-}
+  res.send("login route");
+};
 
 export const logout = async (req, res) => {
-    res.send("logout route")
-}
+  res.send("logout route");
+};
--- test/index.js
+++ test/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { connectDb } from "./db/connectDB.js";
 import  authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
 
+
+dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 8000
+
+
+//allows to parse incomming request wiht json payloads
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello World! 12");
@@ -17,3 +24,5 @@ app.listen(PORT, () => {
   connectDb();
   console.log("Server is running on port :", PORT);
 });
+
+//40.42
