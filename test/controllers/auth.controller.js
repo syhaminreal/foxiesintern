ec688033a3df17514de20266acd15558293c2f10
Reed Monkey <shyaminreallife@gmail.com>
2025-02-11 13:52:45 +0545

    back </?> done

--- test/controllers/auth.controller.js
+++ test/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import { User } from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"; 
-import { sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/emails.js';
-
-
+import { sendPasswordResetEmail, sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/emails.js';
+import crypto from "crypto"
+//sign up
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
   try {
@@ -78,14 +78,76 @@ password: undefined,
 }
  })
   }catch(error){
+    console.log("error in Verification", error)
+    res.status(500).json({sucess:false, message: "Server error"})
 
   }
 }
 
+//login user
 export const login = async (req, res) => {
-  res.send("login route");
+  // res.send("login route");
+  try{
+    const user = await User.findOne({email})
+    if(!user){
+      return res.status(400).json({ sucess: false, message: "Invalid credidentials"})
+
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password)
+    if(!isPasswordValid) {
+      return res.status(400).json({ sucess: false, message: "Invalid credenditals"})
+    }
+    generateTokenAndSetCookie(res, user._id)
+
+    user.lastLogin = new Date()
+    await user.save()
+
+    res.status(200).json({
+      sucess: true,
+      message: "logged in sucessfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      }
+    })
+
+  }catch(error){
+    console.log("Error in loggin",error)
+    res.status(400).json({ sucess: false,  message: error.message})
+
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("logout route");
+  // res.send("logout route");
+  res.clearcookie("token")
+  res.status(200).json({ sucess: true, message: "Logged out sucessfully"})
 };
+
+
+//forgot password
+export const forgotPassword = async (req, res) => {
+  const { email} = req.body
+  try{
+  const user = await user.findOne({ email })
+
+  if(!user){
+    return res.status(400).json({success: false, message: "USer not found"})
+  }
+//generate reset token
+const resetToken = crypto.randomBytes(20).toString("hex")
+const resetTokenExpiresAt =  Date.now() +1 *60*60*1000 //1 hour
+
+user.resetPasswordToken = resetToken
+user.resetPasswordExpiresAt = resetTokenExpiresAt
+
+await user.save()
+
+//send email 
+await sendPasswordResetEmail(user.email, '')
+
+  }catch(error){
+
+  }
+
+}
