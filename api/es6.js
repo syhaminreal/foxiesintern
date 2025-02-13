1955f038ca25a2d0f8767b3d6411ff8d45f77e1f
Reed Monkey <shyaminreallife@gmail.com>
2025-02-13 23:04:04 +0545

    ..

---
+++ api/es6.js
@@ -0,0 +1,24 @@
+// //let and const var is old and let and const are new ways. 
+// let a= 5 //can be able to change the values
+// const  b = 5 //cannot be changed  
+// //the value of the cosnst cannot be assigned later so uneed to asign the vlaue as soon as  the value is inatilized.
+// // add the vlaue as the window object 
+// //both are braces scoped meaing the acess is only possible thorugh the braces
+
+// console.log(a)
+// console.log(b)
+
+
+
+// //arrow function and its necessity
+// //function statment
+// //function expression
+// //anynomous expression
+// //only one type contains 3 difreent ways i.e arrow function with necessary paraments and arugemnts
+// //with one paramenter
+// //with implicit return
+// //normal basic arrow funtion i.e fat arrow function function() => {}
+// //var  a = () ={ conso.log ....}
+// //fat arrow funtion with one parameter
+// // var  b = (params )=> {}
+// //b(12)
