639182e0c7c1682757935a1774e3057ba85e8b44
Reed Monkey <shyaminreallife@gmail.com>
2025-03-05 21:41:25 +0545

    ..

--- reactCourse/vite-project/src/App.jsx
+++ reactCourse/vite-project/src/App.jsx
@@ -4,11 +4,15 @@ const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
+  
     <div style={{ textAlign: "center", marginTop: "20px" }}>
       <h2>Counter: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count + 1)}>Increment value</button>
+
+      <br>
+      </br>
       <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
-        Decrement
+        Decrement value
       </button>
     </div>
   );
