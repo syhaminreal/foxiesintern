f1a545b5b0a2f983070c59563986f0883c0c7ba7
Reed Monkey <shyaminreallife@gmail.com>
2025-03-08 15:54:19 +0545

    ..

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-<h1>Shyam here</h1>
+<h1 className ='bg-green-400'>Shyam here</h1>
     </>
   )
 }
--- react/vite-project/tailwind.config.js
+++ react/vite-project/tailwind.config.js
@@ -1,12 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-      },
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  // ...
-}
+    plugins: [],
+  };
+  
